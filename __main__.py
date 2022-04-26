86d188b3491882828371ed41438d46a513aa9938
Andreea Florescu <fandree@amazon.com>
2022-04-26 12:14:11 +0300

    add a proper command line parser to the tool
    It is still rough around the edges, but this makes the tool way
    easider to use. Also, refactor some of the code in contributors.
    
    Signed-off-by: Andreea Florescu <fandree@amazon.com>

--- __main__.py
+++ __main__.py
@@ -1,15 +1,47 @@
+import argparse
+import datetime
+
 import metrics
 from metrics import contributors
 
 if __name__ == "__main__":
-    year = metrics.get_quarter_of(2019)
-    for q in year:
-        print(q[0])
-        # all_contribs = get_unique_contributors(since=q[1], until=q[2])
-        # amzn_contribs = list(filter(lambda c: "amazon" in c, all_contribs))
-        # print("all contributors:", len(all))
-        # print("amazon contributors:", len(amzn_contribs))
-        # print("-------------------------------------------------")
-        all_commits = contributors.get_commits(q[1], q[2])
-        amzn_commits = list(filter(lambda c: "amazon" in c.commit.author.email, all_commits))
-        print(f"{len(all_commits)} {len(amzn_commits)}")
+    parser = argparse.ArgumentParser(prog='community-metrics', description='Utility that prints community'
+                                                                           'stats using Github')
+    parser.add_argument(dest='action',
+                        type=str,
+                        choices=['commits', 'contributors'],
+                        help='Action to take.')
+
+    parser.add_argument('-g', '--github-org', action='store', type=str, dest='github_org', default='rust-vmm')
+    parser.add_argument('-s', '--start-year', type=str, dest='start_year', default=datetime.date.today().year,
+                        help='Year for which to get the stats. By default the current year is showed.')
+    parser.add_argument('-e', '--end-year', type=str, dest='end_year', default=datetime.date.today().year,
+                        help='Year until when to get the stats. This will by default be set to the current year.')
+    parser.add_argument('-v', '--verbose', help="Print details about the commits/contributors.")
+
+    args = parser.parse_args()
+    org = args.github_org
+    start_year = args.start_year
+    end_year = args.end_year
+    action = args.action
+
+    if end_year < start_year:
+        raise ValueError("The end year cannot be lower than the start year.")
+
+    for year in range(start_year, end_year + 1):
+        quarters = metrics.get_all_quarters_of(year)
+        for q in quarters:
+            if action == "contributors":
+                all_contribs = contributors.get_unique_contributors(since=q[1], until=q[2])
+                amzn_contribs = list(filter(lambda c: "amazon" in c, all_contribs))
+                print(len(all_contribs), len(amzn_contribs), q[0])
+                if args.verbose:
+                    print("All Contributors:", all_contribs)
+                    print("Amazon Contributors:", amzn_contribs)
+            if action == "commits":
+                all_commits = contributors.get_commits(q[1], q[2])
+                amzn_commits = list(filter(lambda c: "amazon" in c.commit.author.email, all_commits))
+                print(len(all_commits), len(amzn_commits), q[0])
+                if args.verbose:
+                    print("All Commits:", all_commits)
+                    print("Amazon Commits:", amzn_commits)
--- metrics/__init__.py
+++ metrics/__init__.py
@@ -16,20 +16,52 @@ def init_github():
     return github
 
 
-rust_vmm_org = init_github().get_organization('rust-vmm')
+def get_org(name='rust-vmm'):
+    return init_github().get_organization(name)
 
 
-def get_repos(org=rust_vmm_org):
-    return org.get_repos()
+def get_repos(org='rust-vmm'):
+    return get_org(org).get_repos()
 
 
 def date_from(str_date):
     return datetime.datetime.strptime(str_date, "%Y-%m-%d")
 
 
-def get_quarter_of(year):
-    Q1 = (f"Q1_{year}", date_from(f"{year}-01-01"), date_from(f"{year}-03-31"))
-    Q2 = (f"Q2_{year}", date_from(f"{year}-04-01"), date_from(f"{year}-06-30"))
-    Q3 = (f"Q3_{year}", date_from(f"{year}-07-01"), date_from(f"{year}-09-20"))
-    Q4 = (f"Q4_{year}", date_from(f"{year}-10-01"), date_from(f"{year}-12-31"))
-    return [Q1, Q2, Q3, Q4]
+def get_all_quarters_of(year):
+    return [q1(year), q2(year), q3(year), q4(year)]
+
+
+def q1(year):
+    return f"Q1_{year}", date_from(f"{year}-01-01"), date_from(f"{year}-03-31")
+
+
+def q2(year):
+    return f"Q2_{year}", date_from(f"{year}-04-01"), date_from(f"{year}-06-30")
+
+
+def q3(year):
+    return f"Q3_{year}", date_from(f"{year}-07-01"), date_from(f"{year}-09-20")
+
+
+def q4(year):
+    return f"Q4_{year}", date_from(f"{year}-10-01"), date_from(f"{year}-12-31")
+
+
+def quarter_year_as_date_interval(quarter, year):
+    """
+    Get the quarter of the year.
+    :param quarter: One of Q1, Q2, Q3, Q4.
+    :param year: The year of the date.
+    :return: Returns a tuple representing the quarter start and end date.
+    """
+    if quarter == "Q1":
+        return q1(year)
+    elif quarter == "Q2":
+        return q2(year)
+    elif quarter == "Q3":
+        return q3(year)
+    elif quarter == "Q4":
+        return q4(year)
+    else:
+        raise ValueError("Invalid quarter specified. Valid options are: Q1, Q2, Q3, and Q4")
